efa1ffe10dc69cd226d16b339f545a5251d00a93
Ma Liulong <marooncn@163.com>
2017-10-23 18:23:08 +0800

    Update NewtonInteration_example.m

--- numerical analysis/NewtonInteration_example.m
+++ numerical analysis/NewtonInteration_example.m
@@ -1,1 +1,24 @@
+f1 = @(x)[x*exp(x)-1];
+x0 = NewtonSe(f1,0.5)
+%{
+1 times iteration.
+The value is 0.571020.
+2 times iteration.
+The value is 0.567156.
+3 times iteration.
+The value is 0.567143.
+
+x =
+
+   0.567143290533261
+%}
+% test
+subs(f1,x)
+% ans =
+ 
+%(51083726 23822543*exp(5108372623822543/9007199254740992))/9007199254740992 - 1
+eval(subs(f1,x))
+% ans =
+
+%     3.411950721954327e-10
 
