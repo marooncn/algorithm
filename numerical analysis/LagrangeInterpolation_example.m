a9fa2de43c2783807539569ca6e30bf2f289c005
Ma Liulong <marooncn@163.com>
2017-10-24 19:14:03 +0800

    Update LagrangeInterpolation_example.m

--- numerical analysis/LagrangeInterpolation_example.m
+++ numerical analysis/LagrangeInterpolation_example.m
@@ -1,13 +1,15 @@
 %For showing Runge's Phenomenon
 X = (-1.0:0.2:1.0);  %interplotion points
 f = @(x)(1.0./(1+25*x.^2));
-Y = f(X);  %the corresponding values of interplotion points
+Y = f(X);   %the corresponding values of interplotion points
 k = 1;
 for i=-1.0:0.03:1.0
-    E(k) = f(i) - LagrangeInterpolation(X,Y,i);
+    L(k) = LagrangeInterpolation(X,Y,i);
+    E(k) = abs(f(i) - L(k));
     k = k+1;
 end
 x = (-1.0:0.03:1.0);
-plot(x,E);
-xlabel('x'),ylabel('E(x)=L(x)-f(x)');
+plot(x,f(x),'b',x,L,'g',x,E,'r');
+legend('f(x)','L(x)','|E(x)|');
+xlabel('x'),ylabel('y');
 title("Runge's Phenomenon");
