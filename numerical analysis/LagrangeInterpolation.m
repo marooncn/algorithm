0b3a8d60893134475a7597c4ee3b5eccc979aca6
Ma Liulong <marooncn@163.com>
2017-10-24 18:56:39 +0800

    Create LagrangeInterpolation.m

---
+++ numerical analysis/LagrangeInterpolation.m
@@ -0,0 +1,19 @@
+function y = LagrangeInterpolation(X,Y,xh)
+%Lagrange Interpolation, 
+%X and Y is interpolation dot and corresponding value.
+n = length(X);
+if ( length(Y) ~= n)
+    fprintf("The input two vectors aren't the same size!");
+    return ;
+end
+y = 0;
+for i=1:n
+    l(i) = 1;
+    for j=1:n
+        if(j~=i)
+            l(i) = l(i)*(xh-X(j))*1.0/(X(i)-X(j));
+        end
+    end
+    y = y + l(i)*Y(i);
+end
+end
