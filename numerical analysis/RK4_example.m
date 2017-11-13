724e0918b3745ccd1d73b59f5a49597ddee62f9b
Ma Liulong <marooncn@163.com>
2017-11-13 13:58:35 +0800

    Update RK4_example.m

--- numerical analysis/RK4_example.m
+++ numerical analysis/RK4_example.m
@@ -1,12 +1,12 @@
 f = @(x,y)[3*x-y];
 yn = RK4(f,0,1,10)
-% y(1.000000)=1.471519
-% yn =
 
-%     1.4715
 % if using function to define f, for example
 % function f = fx(x,y)
 % f = 3*x-y;
 % end
 % then use function handle to transfer
 % yn = RK4(@fx,0,1,10)
+
+% Compare with Euler Method
+% yn = Euler(f,0,1,10)
